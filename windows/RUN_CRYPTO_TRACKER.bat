723fd29bd9dfc1bac24dd7b540942af6cb335347
taoteh1221 <mike@dragonfrugal.com>
2023-11-06 19:01:12 -0500

    Clear browser temporary files on startup (windows/phpbrowserbox)
    Clear browser temporary files on startup (windows/phpbrowserbox)

--- windows/RUN_CRYPTO_TRACKER.bat
+++ windows/RUN_CRYPTO_TRACKER.bat
@@ -1,2 +1,20 @@
-cd "bin\bbwebkit"
-start "" bbwebkit.exe
+@echo off
+
+
+:: Set paths
+set browser_dir="bin\bbwebkit"
+set temp_cache="bin\bbwebkit\user-dir\Default\Cache\Cache_Data"
+
+
+:: ONLY DELETE FILES WITH THE PREFIX: data_
+cd /d %temp_cache%
+for /F "delims=" %%i in ('dir *data_* /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)  >nul 2>&1
+
+
+:: Give the cache a small breather
+timeout 2
+
+
+:: Fire up the browser
+cd /d ..\..\..\..\..\..\%browser_dir%
+start "" bbwebkit.exe --aggressive-cache-discard --enable-aggressive-domstorage-flushing  >nul 2>&1
